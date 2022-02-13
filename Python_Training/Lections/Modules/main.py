fca0391cda69a6a4392fb1190acbd1a88f213101
OvcS <gerkules007@yahoo.com>
2022-02-13 19:45:05 +0300

    main complete

--- Python_Training/Lections/Modules/main.py
+++ Python_Training/Lections/Modules/main.py
@@ -1,6 +1,7 @@
 import html_creater as hc
+import xml_creater as xml
 import data_provider as dt
 
 # print(hc.create())
 
-hc.create_collection(dt.data_collection())
+xml.create_collection(hc.create_collection(dt.data_collection()))
