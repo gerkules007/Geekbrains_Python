40334fcfbda6fff5563eb5aba5227804c0fd6c22
OvcS <gerkules007@yahoo.com>
2022-02-13 19:19:43 +0300

    Add cortage  html_creater

--- Python_Training/Lections/Modules/html_creater.py
+++ Python_Training/Lections/Modules/html_creater.py
@@ -14,3 +14,20 @@ def create(device = 1):
     with open('Python_Training/Lections/Modules/index.html', 'w') as page:
         page.write(html)
     return html
+
+def create_collection(data, device = 1):
+    t, p, w = data
+    t = t + 32
+    style = 'style="font-size:22px;"'
+    html = '<html>\n <head></head>\n <body>\n'
+    html += '   <p {}>Temperature: {} F</p>\n'\
+        .format(style, t)
+    html += '   <p {}>Wind_speed: {} m/s</p>\n'\
+        .format(style, w) 
+    html += '   <p {}>Pressure: {} mmHg</p>\n'\
+        .format(style, p)
+    html += ' </body>\n</html>\n'
+
+    with open('Python_Training/Lections/Modules/index.html', 'w') as page:
+        page.write(html)
+    return data
